657d7660e49ac6c982ec808340f4f6948b551b7b
Ahumaraeze Princewill <princewillahumaraeze@gmail.com>
2025-02-24 01:25:50 +0100

    Added Rust Implementation

--- rust/leftpad/src/main.rs
+++ rust/leftpad/src/main.rs
@@ -1,3 +1,12 @@
+fn leftpad(s: &str, length: usize,  pad: &str){
+	if length <= s.len(){
+		println!("{s}");
+		return 
+	}
+	let padding  = pad.repeat(length - s.len());
+	println!("{}", padding+s);
+}
+
 fn main() {
-    println!("Hello, world!");
+    leftpad("Foo", 5, "*");
 }
